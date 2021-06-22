f3e6dd2c93000e514130a7048cb1340734fd79b6
mdestafadilah <desta.08b@gmail.com>
2021-06-23 03:54:54 +0700

    Stop Not-interesting

---
+++ src/Quiz.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Quiz() {
+    return (
+        <div className="quiz">
+            <div className="quiz-content">
+                What is the sum of 5 and 5?
+                
+            </div>
+        </div>
+    )
+}
